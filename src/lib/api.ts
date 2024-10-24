3261561d2fe0a6c36cff1a2e6ce90eedd52311cc
IanCoolidge0 <coolidgeian15@gmail.com>
2024-10-24 08:28:39 -0400

    dynamic env variables so i can maybe deploy

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,14 +1,14 @@
-import { NODE_ENV, STRAVA_CLIENT_ID, STRAVA_CLIENT_SECRET } from "$env/static/private";
+import { env } from '$env/dynamic/private';
 import type { ActivityStats, StravaSession, SummaryActivity } from "./types";
 
 export const getStravaRedirectURI = () => {
-    const prefix = (NODE_ENV === 'development') ? 'http://localhost:5173' : 'http://localhost:4173';
+    const prefix = (env.NODE_ENV === 'development') ? 'http://localhost:5173' : 'http://localhost:4173';
     return `${prefix}/auth/redirect`;
 }
 
 export const getStravaAuthorizeURI = (redirectUri: string, scopes: string[], force?: boolean) => {
     let url = `https://strava.com/oauth/authorize`;
-    url = `${url}?client_id=${STRAVA_CLIENT_ID}`;
+    url = `${url}?client_id=${env.STRAVA_CLIENT_ID}`;
     url = `${url}&redirect_uri=${redirectUri}`;
     url = `${url}&response_type=code`;
     url = `${url}&approval_prompt=${force ? 'force' : 'auto'}`;
@@ -18,8 +18,8 @@ export const getStravaAuthorizeURI = (redirectUri: string, scopes: string[], for
 
 export const getStravaSession = async (code: string, grantType: 'authorization_code' | 'refresh_token') => {
     let url = `https://strava.com/api/v3/oauth/token`;
-    url = `${url}?client_id=${STRAVA_CLIENT_ID}`;
-    url = `${url}&client_secret=${STRAVA_CLIENT_SECRET}`;
+    url = `${url}?client_id=${env.STRAVA_CLIENT_ID}`;
+    url = `${url}&client_secret=${env.STRAVA_CLIENT_SECRET}`;
     url = `${url}&grant_type=${grantType}`;
     url = `${url}&${grantType === 'authorization_code' ? 'code' : 'refresh_token'}=${code}`;
     const response = await fetch(url, {
